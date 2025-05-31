4dbfc4a25052305da0a56f98d125b621b1d09906
kabir <kabirj2505@gmail.com>
2025-05-31 12:01:16 +0530

    fix CHV pages

--- disaster-maangement-app/app/routes/__init__.py
+++ disaster-maangement-app/app/routes/__init__.py
@@ -2,12 +2,13 @@ from flask import Blueprint
 
 from .auth import auth_bp
 
-# from .admin import admin_bp
+from .model import model_bp
 # from .public import public_bp
 from .home import home_bp
 
 def register_blueprints(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(home_bp)
+    app.register_blueprint(model_bp)
     
   
--- disaster-maangement-app/app/routes/model.py
+++ disaster-maangement-app/app/routes/model.py
@@ -1,1 +1,16 @@
 
+from flask import Blueprint, render_template
+
+model_bp = Blueprint('model', __name__)
+
+@model_bp.route('/cyclone')
+def modelC():
+    return render_template('modelC.html')
+
+@model_bp.route('/hurricane')
+def modelH():
+    return render_template('modelH.html')
+
+@model_bp.route('/volcano')
+def modelV():
+    return render_template('modelV.html')
